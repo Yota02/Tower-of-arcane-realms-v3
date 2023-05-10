1a8fb13f2eae4b58a4a5cec9d256ef76a757effc
Yota002 <amichauxkinet@gmail.com>
2023-05-10 14:55:41 +0200

    Ajout des commentaires #

--- inventaire.py
+++ inventaire.py
@@ -1,29 +1,26 @@
 class Inventory:
     def __init__(self):
-        self.capacity = 100
-        self.items = []
+        self.capacity = 100  # Capacité maximale de l'inventaire
+        self.items = []  # Liste des objets dans l'inventaire
 
     def ajouter_objet(self, objet):
-        if len(self.objets) < self.capacite:
-            self.objets.append(objet)
-            print(f"{objet.nom} a été ajouté à l'inventaire.")
+        if len(self.objets) < self.capacite:  # Vérifier si l'inventaire est plein
+            self.objets.append(objet)  # Ajouter l'objet à la liste des objets
+            print(f"{objet.nom} a été ajouté à l'inventaire.")  # Afficher un message de confirmation
         else:
-            print("L'inventaire est plein.")
+            print("L'inventaire est plein.")  # Afficher un message d'erreur si l'inventaire est plein
 
     def supprimer_objet(self, objet):
-        if objet in self.objets:
-            self.objets.remove(objet)
-            print(f"{objet.nom} a été retiré de l'inventaire.")
+        if objet in self.objets:  # Vérifier si l'objet est dans l'inventaire
+            self.objets.remove(objet)  # Retirer l'objet de la liste des objets
+            print(f"{objet.nom} a été retiré de l'inventaire.")  # Afficher un message de confirmation
         else:
-            print(f"{objet.nom} n'est pas dans l'inventaire.")
+            print(f"{objet.nom} n'est pas dans l'inventaire.")  # Afficher un message d'erreur si l'objet n'est pas dans l'inventaire
 
     def afficher_inventaire(self):
-        print("Inventaire :")
+        print("Inventaire :")  # Afficher le titre de l'inventaire
         if len(self.objets) == 0:
-            print("Vide")
+            print("Vide")  # Afficher un message si l'inventaire est vide
         else:
             for objet in self.objets:
-                print(f"{objet.nom} - {objet.description}")
-
-
-
+                print(f"{objet.nom} - {objet.description}")  # Afficher le nom et la description de chaque objet dans l'inventaire
--- item.py
+++ item.py
@@ -3,15 +3,16 @@ class Item:
         self.name = name
         self.description = description
 
-
 class Weapon(Item):
     def __init__(self, name, description, damage, durability):
         super().__init__(name, description)
         self.damage = damage
         self.durability = durability
 
-
 class Potion(Item):
     def __init__(self, name, description, effet):
         super().__init__(name, description)
         self.effet = effet
+
+epee_acier = Weapon("Épée en acier", "Une épée forgée en acier", 10, 50)
+
